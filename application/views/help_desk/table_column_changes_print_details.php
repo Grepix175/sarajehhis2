1411d0273c41b6cb6cd7ed5da3bd6f3aff34cd0d
mohit1591 <130342993+mohit1591@users.noreply.github.com>
2024-11-20 11:27:47 +0530

    Help desk print history,Refraction print page and hess chart print page changes done

---
+++ application/views/help_desk/table_column_changes_print_details.php
@@ -0,0 +1,59 @@
+<!--  hms_std_eye_report_print_setting changes page_details column chnage table code this New code -->
+<table border="0" cellpadding="0" cellspacing="0" style="width:100%;border:2px solid #ccc;font-size:12px;">
+	<tbody>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Patient</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {patient_name}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Mobile no.</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {mobile_no}</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Patient.Reg.No</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {patient_reg_no}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Age</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {age}</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Token No</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {token_no}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Gender</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {gender}</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">OPD No</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {booking_code}</td>
+			<!-- <th align="left" style="padding:2px 2px;" valign="top" width="15%">Appt. Dt</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {booking_date}</td> -->
+		</tr>
+	</tbody>
+</table>
+<!--  hms_std_eye_report_print_setting changes page_details column chnage table code this old code -->
+<table border="0" cellpadding="0" cellspacing="0" style="width:100%;border:2px solid #ccc;font-size:12px;">
+	<tbody>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Patient</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {patient_name}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Doctor</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {doctor_name}</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Age/Sex</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {patient_age}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Facility</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: sure vision eye hospitals</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Pat ID</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {patient_reg_no}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Appt. ID</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {app_id}</td>
+		</tr>
+		<tr>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Contact</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {mobile_no}</td>
+			<th align="left" style="padding:2px 2px;" valign="top" width="15%">Appt. Dt</th>
+			<td align="left" style="padding:2px 2px;" valign="top" width="35%">: {booking_date}</td>
+		</tr>
+	</tbody>
+</table>
+
